d14cef132d9c6d520e5e1bee5dbf6bf29ea00a89
Pablo Moreno <pablo.a.moreno@gmail.com>
2019-04-01 14:18:51 +0100

    Improve error handling on label check

--- run_galaxy_workflow.py
+++ run_galaxy_workflow.py
@@ -172,7 +172,7 @@ def load_input_files(gi, inputs, workflow, history):
                     'src': 'hda'
                 }
         else:
-            raise ValueError("Label % is not present in inputs yaml %" % (step_data['label'], inputs))
+            raise ValueError("Label '{}' is not present in inputs yaml".format(step_data['label']))
 
     return inputs_for_invoke
 
