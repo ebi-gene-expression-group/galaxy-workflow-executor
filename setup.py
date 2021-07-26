d11fe23b549dd8671466d1a6979c074e7aced8d3
Jonathan Manning <jmanning@ebi.ac.uk>
2021-07-26 14:39:37 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ print(find_packages())
 
 setup(
         name='galaxy-workflow-executor',
-        version='0.2.4',
+        version='0.2.5',
         description='Execute workflows on Galaxy through the CLI',
         long_description=readme(),
         packages=find_packages(),
