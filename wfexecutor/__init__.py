d0c988fffb5f3d57e924a544aca9ad3597ee0524
Anil Thanki <thanki.anil@gmail.com>
2023-05-12 16:27:22 +0100

    adds dataset header option

--- wfexecutor/__init__.py
+++ wfexecutor/__init__.py
@@ -182,6 +182,8 @@ def load_input_files(gi, inputs, workflow, history):
             upload_res = gi.tools.upload_file(path=inputs[step_data['label']]['path'], history_id=history['id'],
                                               file_name=step_data['label'],
                                               file_type=inputs[step_data['label']]['type'])
+            gi.datasets.update_dataset(upload_res['outputs'][0]['id'], name=step_data['label'], meta={'header': True})
+            
             inputs_for_invoke[step] = {
                     'id': upload_res['outputs'][0]['id'],
                     'src': 'hda'
